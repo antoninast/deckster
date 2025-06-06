f55a05233bf2de0a3a6a2a6ec9ea9fae02d776bb
Antonina S <antoninad22@gmail.com>
2025-06-06 00:08:24 -0500

    enhance css for ManageFlashcards and BrowseDecks

--- client/src/components/IndividualDeck/IndividualDeck.tsx
+++ client/src/components/IndividualDeck/IndividualDeck.tsx
@@ -1,5 +1,6 @@
 import { FaCog, FaBookOpen, FaFileImport, FaLock } from "react-icons/fa";
 import { MdPublic } from "react-icons/md";
+import { GrTrash } from "react-icons/gr";
 import { CardDeck } from "../../interfaces/CardDeck";
 import "./IndividualDeck.css";
 
@@ -34,11 +35,7 @@ const IndividualDeck = ({
                 onClick={() => handleRemoveCardDeck(deck._id)}
                 aria-label="Delete deck"
               >
-                <svg xmlns="http://www.w3.org/2000/svg"
-                  width="17" height="17" fill="gray" className="bi bi-x-square delete-icon" viewBox="0 0 16 16">
-                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                </svg>
+                <GrTrash />
               </div>
             )}
           </div>
@@ -73,7 +70,7 @@ const IndividualDeck = ({
               <span className="deck-stat-label">Visibility:</span>
               <div className="dropdown">
                 {deck.isPublic ? <MdPublic /> : <FaLock />}
-                {user._id === deck.user._id ?
+                {user?._id === deck.user._id ?
                   <>
                     <button className="btn dropdown-toggle visibility" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
                     <ul className="dropdown-menu">
--- client/src/components/ManageFlashcards/ManageFlashcards.tsx
+++ client/src/components/ManageFlashcards/ManageFlashcards.tsx
@@ -6,6 +6,8 @@ import {
   //   QUERY_CARD_DECKS,
 } from "../../utils/queries";
 import { REMOVE_FLASHCARD, UPDATE_FLASHCARD } from "../../utils/mutations";
+import { GrEdit, GrTrash } from "react-icons/gr";
+import { RiSave3Line } from "react-icons/ri";
 import type { Flashcard } from "../../interfaces/Flashcard";
 import "./ManageFlashcards.css";
 
@@ -189,17 +191,7 @@ export default function Flashcards() {
               className="flashcard-delete-button-wrapper"
               onClick={() => handleRemoveFlashcard(flashcard._id)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                fill="gray"
-                className="bi bi-x-square delete-icon"
-                viewBox="0 0 16 16"
-              >
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-              </svg>
+              <GrTrash />
             </div>
             {flashcard._id === currentlyEditedCardId ? (
               <div className="edit-mode">
@@ -221,29 +213,20 @@ export default function Flashcards() {
                 <p className="card-answer">{flashcard.answer}</p>
               </div>
             )}
-            <div className="action-buttons">
-              <div
-                onClick={() =>
+            <div
+              className="flashcard-action-buttons"
+              onClick={() =>
                   handleUpdateFlashcard(
                     flashcard._id,
                     flashcard.question,
                     flashcard.answer
                   )
-                }
-              >
+                }>
                 {flashcard._id === currentlyEditedCardId ? (
-                  <button type="button" className="btn btn-outline-info btn-sm">
-                    💾 Save
-                  </button>
+                  <RiSave3Line className="save-btn"/>
                 ) : (
-                  <button
-                    type="button"
-                    className="btn btn-outline-secondary btn-sm"
-                  >
-                    ✏️ Edit
-                  </button>
+                  <GrEdit />
                 )}
-              </div>
             </div>
           </div>
         ))}
